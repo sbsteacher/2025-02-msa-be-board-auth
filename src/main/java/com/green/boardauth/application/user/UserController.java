a08dbade512b851f57c2017b152e920ee8613b64
sbsteacher <id@gmail.com>
2026-02-05 11:58:16 +0900

    board

---
+++ src/main/java/com/green/boardauth/application/user/UserController.java
@@ -0,0 +1,12 @@
+package com.green.boardauth.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+}
---
+++ src/main/java/com/green/boardauth/application/user/UserService.java
@@ -0,0 +1,10 @@
+package com.green.boardauth.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper userMapper;
+}
