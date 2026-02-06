61f969c2f9150b64e91b06803ff2f59a3c9ec486
sbsteacher <id@gmail.com>
2026-02-06 16:38:48 +0900

    JwtTokenProvider-9

--- src/main/java/com/green/boardauth/application/board/BoardController.java
+++ src/main/java/com/green/boardauth/application/board/BoardController.java
@@ -2,8 +2,10 @@ package com.green.boardauth.application.board;
 
 import com.green.boardauth.application.board.model.BoardPostReq;
 import com.green.boardauth.configuration.model.ResultResponse;
+import com.green.boardauth.configuration.model.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     private final BoardService boardService;
 
-
+    //import com.green.boardauth.configuration.model.UserPrincipal;
     @PostMapping
-    public ResultResponse<?> postBoard(@RequestBody BoardPostReq req) {
+    public ResultResponse<?> postBoard(@AuthenticationPrincipal UserPrincipal userPrincipal
+                                     , @RequestBody BoardPostReq req) {
+        log.info("통신됐다!!");
+        log.info("signedUserId: {}", userPrincipal.getSignedUserId());
         log.info("req: {}", req);
         return null;
     }
--- src/main/java/com/green/boardauth/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/green/boardauth/configuration/security/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.green.boardauth.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,11 @@ public class WebSecurityConfiguration {
                 .httpBasic( hb -> hb.disable() )  //시큐리티에서 제공해주는 로그인 화면이 있는데 사용하지 않겠다
                 .formLogin( fl -> fl.disable() ) //어차피 BE가 화면을 만들지 않기 때문에 formLogin기능도 비활성화하겠다.
                 .csrf( csrf -> csrf.disable() ) //어차피 BE가 화면을 만들지 않으면 csrf 공격이 의미가 없기 때문에 비활성화하겠다.
+                //인가처리 (권한처리)
+
+                //아래 내용은 (POST) /api/board 로 요청이 올 때는 반드시 로그인이 되어있어야 한다.
+                .authorizeHttpRequests( req -> req.requestMatchers(HttpMethod.POST, "/api/board").authenticated() )
+
                 .build();
     }
 
