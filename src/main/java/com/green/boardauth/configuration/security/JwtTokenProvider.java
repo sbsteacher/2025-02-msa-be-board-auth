2476201074e03ff1c25841184e16ddc861559c23
sbsteacher <id@gmail.com>
2026-02-06 10:54:15 +0900

    JwtTokenProvider-1

--- src/main/java/com/green/boardauth/configuration/security/JwtTokenProvider.java
+++ src/main/java/com/green/boardauth/configuration/security/JwtTokenProvider.java
@@ -1,12 +1,16 @@
 package com.green.boardauth.configuration.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.green.boardauth.configuration.constants.ConstJwt;
+import com.green.boardauth.configuration.model.JwtUser;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Slf4j
 @Component //역할이 없는 빈등록
@@ -21,6 +25,35 @@ public class JwtTokenProvider {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(constJwt.getSecretKey()));
 
         log.info("constJwt: {}", this.constJwt);
+    }
+
+    public String generateAccessToken(JwtUser jwtUser) {
+        return "";
+    }
+
+    public String generateRefreshToken(JwtUser jwtUser) {
+        return "";
+    }
+
+    //JWT 문자열을 만드는 메소드
+    public String generateToken(JwtUser jwtUser, long tokenValidityMilliSeconds) {
+        Date now = new Date(); // import java.util.Date;
+        return Jwts.builder()
+                //header
+                .header().type(constJwt.getBearerFormat()) //JWT
+                .and()
+                //payload
+                .issuer( constJwt.getIssuer() )
+                .issuedAt( now )  //JWT만든 일시 (토큰 생성일시)
+                .expiration( new Date(now.getTime() + tokenValidityMilliSeconds) ) //JWT종료 일시 (토큰 만료일시)
+                .claim( constJwt.getClaimKey(), makeClaimByUserToJson(jwtUser) )
+
+                .signWith(secretKey) //signature
+                .compact();
+    }
+
+    public String makeClaimByUserToJson(JwtUser jwtUser) { //직렬화 (Serialization)
+        return objectMapper.writeValueAsString(jwtUser);  //객체 > JSON문자열로 변환
     }
 
 
